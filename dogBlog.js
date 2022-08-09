08c0dcc93339bd5b48e98d020be9524ba87c1a1f
Nikki W <86427717+anikki1@users.noreply.github.com>
2022-08-09 17:10:40 -0400

    adding new pages

---
+++ dogBlog.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.getElementById('navigation')
+    nav.innerHTML = '<ul> <li> <a href="dogBlog.html"> home </a> </li> <li> settings</li> <li> <a href="activities.html"> profile </a></li></ul>'
+    })
+    
